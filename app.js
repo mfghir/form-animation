3ca5fa43e75d37a9d13f02aa376b827dae89192a
miss-f-g <fatemeghafari98@gmail.com>
2022-07-27 11:17:47 +0430

    6.validation inputs

--- app.js
+++ app.js
@@ -57,5 +57,54 @@ form.addEventListener("click", () => {
         });
       }
     }
+
+    //validation =>
+    //Name Validation
+    input.addEventListener("input", (e) => {
+      if (e.target.type === "text") {
+        let inputText = e.target.value;
+        if (inputText.length > 2) {
+          colorize("#6391E8", line, placeholder);
+        } else {
+          colorize("#FE8C99", line, placeholder);
+        }
+      }
+
+      //Validate Email
+      if (e.target.type === "email") {
+        let valid = validateEmail(e.target.value);
+        if (valid) {
+          colorize("#6391E8", line, placeholder);
+        } else {
+          colorize("#FE8C99", line, placeholder);
+        }
+      }
+
+      //Validate Phone
+      if (e.target.type === "tel") {
+        let valid = validatePhone(e.target.value);
+        if (valid) {
+          colorize("#6391E8", line, placeholder);
+        } else {
+          colorize("#FE8C99", line, placeholder);
+        }
+      }
+    });
   });
 });
+
+// checking email validation & phone
+function validateEmail(email) {
+  let re = /\S+@\S+\.\S+/;
+  return re.test(email);
+}
+function validatePhone(phone) {
+  let re = /^[\+]?[(]?[0-9]{3}[)]?[-\s\.]?[0-9]{3}[-\s\.]?[0-9]{4,6}$/im;
+  return re.test(phone);
+}
+
+//COLORIZE FUNCTION
+function colorize(color, line, placeholder) {
+  gsap.to(line, { stroke: color, duration: 0.75 });
+  gsap.to(placeholder, { color: color, duration: 0.75 });
+}
